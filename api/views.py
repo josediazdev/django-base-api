daee9fcc676029435fc3b5d6bbeb1c3afc60c8ea
jdiazdev <josediazmat10@gmail.com>
2025-02-18 12:47:45 -0400

    Agregando validación de autenticación por clave API a los endpoints correspondientes

--- api/views.py
+++ api/views.py
@@ -2,11 +2,13 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import Employees
 from .serializers import EmployeesSerializer
+from rest_framework_api_key.permissions import HasAPIKey
 
 # Create your views here.
 class EmployeesListCreate(generics.ListCreateAPIView):
     queryset = Employees.objects.all()
     serializer_class = EmployeesSerializer
+    permission_classes = [HasAPIKey]
 
     def delete(self, request, *args, **kwargs):
         Employees.objects.all().delete()
@@ -17,3 +19,4 @@ class EmployeesRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employees.objects.all()
     serializer_class = EmployeesSerializer
     lookup_field = "pk"
+    permission_classes = [HasAPIKey]
